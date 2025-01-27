22dd93a26f9b2f274a528d33bc838150b3aa90d7
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-01-27 20:22:01 -0300

    Otimizando o textoFinal

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let listaDeAmigos = []
 
+function textoFinal(texto){
+        let textoFinal = document.getElementById('resultado');
+        textoFinal.innerHTML = texto;
+}
 
 function adicionarAmigo(){
     let nome = document.querySelector('input').value;
@@ -9,24 +11,27 @@ function adicionarAmigo(){
         alert('Por favor, insira um nome.');
     }else{
         listaDeAmigos.push(nome);
-        console.log(listaDeAmigos);
         document.querySelector('input').value = '';
 
         let listagem = document.querySelector('ul');
         let item =  document.createElement('li');
         item.innerHTML = nome;
         listagem.append(item);
+        textoFinal('')
     }
 }
 
 function escolhaDeAmigo(){
-    let num = Math.floor(Math.random()*listaDeAmigos.length);
-    let amigoSecreto = listaDeAmigos[num];
-    let listagem = document.querySelector('ul');
-    listagem.innerHTML = '';
-    let textoFinal = document.getElementById('resultado');
-    textoFinal.innerHTML = `O seu amigo secreto é ${amigoSecreto}`
-    console.log(amigoSecreto);
-}
+    if (listaDeAmigos != '') {
+        let pick = Math.floor(Math.random()*listaDeAmigos.length);
+        let amigoSecreto = listaDeAmigos[pick];
+        let listagem = document.querySelector('ul');
+        listagem.innerHTML = '';
+        textoFinal(`O seu amigo secreto é ${amigoSecreto}`)
+        listaDeAmigos = []
+    } else {
+        alert('Falta inserir os amigos')
+    };
+};
 
 
